75b5f876108d8b6f18abad8b268522b46b2c3c3d
Kapil Rana <kapilrrana@gmail.com>
2021-06-01 11:13:04 +0530

    initial commit

---
+++ eclipse-workspace/new1/src/arrays/BubbleSort.java
@@ -0,0 +1,41 @@
+package arrayS;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		 
+		 int a[] = {-2, 0, 1,5,7};
+		 int n = a.length;
+		 boolean sorted = true;
+		 for(int i = 0; i < n-1; i++) {
+			
+		
+			 
+			 for(int j = 0; j < n-1-i; j++) {
+				 
+				 if(a[j+1] < a[j]) {
+				
+					 int temp = a[j+1];
+					 a[j+1] = a[j];
+					 a[j]  = temp;
+					 
+					 sorted = false;
+				 }
+			 }
+		        if(sorted) break;
+		 }
+
+		 // using for each loop					 
+		 for(int item : a) {
+			 System.out.print(item + " ");
+		 }
+	
+//		 for(int i = 0; i < n; i++) {
+//				int item;
+//			 item = a[i];
+//			 System.out.print(item + " ");
+//		 }
+			 
+	
+	}
+}
